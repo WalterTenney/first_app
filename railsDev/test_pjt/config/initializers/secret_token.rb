42fb5c6069dd9597d9f682576d975170e3a4f334
Walt Tenney <tenneywa@gmail.com>
2011-08-14 22:57:11 -0400

    Initial commit

---
+++ railsDev/test_pjt/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestPjt::Application.config.secret_token = '8d9c334a715c55fae375a02fe5a95664989112517980ddba5c339d3e29f2a4112be168458ce376124f8951c0e24539464ca3ee50483caf066f624f8296da39a6'
